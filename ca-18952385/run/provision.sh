4819dc389e5011058e8f6d0c2fd5b5306327f2e8
simplehsu <simplehsu@gmail.com>
2023-02-21 17:51:30 +0800

    Upgrade the platform to ubuntu2204 and upgrade the related packages as well

--- ca-18952385/run/provision.sh
+++ ca-18952385/run/provision.sh
@@ -5,7 +5,7 @@ UART=/dev/ttyUSB0
 BAUDRATE=115200
 
 WORK_DIR=ca-18952385
-SCRIPT=/home/mt/cert_signer/ca_18952385/18952385_Provisioner
+SCRIPT=/home/mt/cert_signer/ca_18952385/DeviceCertificateProvisioner
 YUBIHSM2_CONF_DIR=/home/mt/mt/src/embedded/hsm-$YUBIHSM2_KEY-config
 
 # LOG PATH
@@ -18,6 +18,7 @@ set -e
 --baud=$BAUDRATE \
 --com_port="$UART" \
 --connector_url="http://127.0.0.1:12345" \
+# --debug=1 \
  2>&1) | tee $LOG_PATH_PROVISION/log-$WORK_DIR-$NOW-provision.log
 
 set +e
